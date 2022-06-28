d38c2195c6822d5964eaf6d0b8a80a922d9bf96d
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-27 22:02:59 -0400

    Created getBooks function that is invoked using componentDidMount. Mapped through and rendered all books found in the database using Carousel.

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import Alert from 'react-bootstrap/Alert';
+import Carousel from 'react-bootstrap/Carousel'
+import Book from './Book'
+
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -8,34 +12,43 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
-getBooks = async () =>{
+  getBooks = async () =>{
   try{
-    let results = await axios.get(`${SERVER}/books`);
+    let results = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
     this.setState({
       books: results.data
     })
   } catch(error){
     console.log('we have an error getting books', error.response.data)
   }
-}
-componentDidMount(){
-  this.getBooks();
-}
-  render() {
-    console.log(this.state.books);
-    
+  }
 
-    /* TODO: render all the books in a Carousel */
+  componentDidMount(){
+  this.getBooks();
+  }
+
+  render() {    
+    let books=this.state.books.map(book=>{
+      return <Book
+      key={book._id}
+      title={book.title}
+      desc={book.description}
+      ></Book>
+    });
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <>
+            <h2>Books</h2>
+            <Carousel>
+              {books}
+            </Carousel>
+          </>
         ) : (
-          <h3>No Books Found :(</h3>
+          <Alert variant="danger">Book collection empty.</Alert>
         )}
       </>
     )
